a2726a23d296655d2fb206f359dc3b8137dc7b8c
iken <iken.krueger@gmx.de>
2018-12-29 16:57:36 +0100

    filter changes

--- frontend/php/transmitter.php
+++ frontend/php/transmitter.php
@@ -6,5 +6,29 @@
 <body class="grid">
 
 <?php include ("menu.php"); ?>
+
+
+    <div class="content">
+        <!--Dropdown Herde, Dropdown Sender als filter -> Ausgabe ist Liste mit Tieren und Status
+        Button zum hinzufügen von Herden/Tieren -> Neue felder erscheinen/überblenden?/Formular -->
+        <div class="dd_herd">
+            <select id="select_herd" onchange="selected()">
+                <option value="h1">Herde 1</option>
+                <option value="h2">Herde 2</option>
+                <option value="h3">Herde 3</option>
+                <option value="h4">Herde 4</option>
+            </select>
+        </div>
+
+        <div class="dd_transmitter">
+            <select id="select_transmitter" onchange="selected()">
+                <option value="t1">Sender 1</option>
+                <option value="t2">Sender 2</option>
+                <option value="t3">Sender 3</option>
+                <option value="t4">Sender 4</option>
+            </select>
+        </div>
+
+    </div>
 </body>
 </html>
