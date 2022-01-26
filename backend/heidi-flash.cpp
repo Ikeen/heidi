7a9d2dede41470b57ac27d4adb97e6847466cd0b
frankheisig71 <f.heisig@online.de>
2022-01-26 16:27:07 +0100

    test flash settings

--- backend/heidi-flash.cpp
+++ backend/heidi-flash.cpp
@@ -12,197 +12,285 @@
 
 t_flashData flashData;
 t_flashRawData flashRawData;
+t_heidiMenu currentMenu = HEIDI_MENU_MAIN;
 
 void setupFlashData(bool powerOnReset){
-  if(!powerOnReset) { return; }
+  if (EEPROM.begin(sizeof(t_flashRawData) * 2)){
+    if (!loadFlashData){ loadDefaultData(&flashData); }
+    if (powerOnReset) { handleHook(); }
+    EEPROM.end();
+  }
+}
+
+void loadDefaultData(t_flashData* flashData){
+  flashData->animalID = HEIDI_ANIMAL;
+  flashData->herdeID = HEIDI_HERDE;
+  flashData->pushDataWebAddress = HEIDI_SERVER_PUSH_URL;
+  flashData->mobileAPN = HEIDI_MOBILE_APN;
+  flashData->mobileUSER = HEIDI_MOBILE_USER;
+  flashData->mobilePASSWD = HEIDI_MOBILE_PASSWD;
+  flashData->mobilePIN = HEIDI_SIM_PIN;
+}
+
+void handleHook(){
   int t = millis();
-  #if DEBUG_LEVEL > DEBUG_LEVEL_0
+  #if DEBUG_LEVEL == DEBUG_LEVEL_0
   Serial.begin(115200);
   #endif
   Serial.println("enter hook: press 'x'");
   String Rs232in = "";
   Serial.flush();
-  while((Rs232in.indexOf("x") == -1) && (millis()-t < 3000)){
+  while((Rs232in.indexOf("x") == -1) && (millis()-t < 10000)){
     if (Serial.available() > 0) {Rs232in += (char)Serial.read();}
     delay(1);
   }
-  if (Rs232in.indexOf('x') == -1){ return; }
-  Serial.println("hook entered.");
-
-  if (!EEPROM.begin(sizeof(t_flashRawData))){
-    Serial.println("Failed to setup EEPROM with " + String(sizeof(t_flashRawData)) + " bytes size.");
-    return;
-  }
-  Serial.println("EEPROM set up");
-  loadFlashData(&flashData, &flashRawData);
-  printFlashDataMenu(&flashData);
-  char key = ' ';
-  while(key != 'q'){
-    key = getSerialKey();
-    bool timeout = false;
-    switch(key) {
-      case '1':{
-        String NewValue = readNewValue("herd ID", 10, &timeout);
-        if (!timeout) { flashData.herdeID = int16_t(NewValue.toInt()); }
-        break;
-      }
-      case '2':{
-        String NewValue = readNewValue("animal ID", 10, &timeout);
-        if (!timeout) { flashData.animalID = int16_t(NewValue.toInt()); }
-        break;
-      }
-      //#ifdef GSM_MODULE
-      case '3':{
-        String NewValue = readNewValue("push data web address", 10, &timeout);
-        if (!timeout) { flashData.pushDataWebAddress = NewValue; }
-        break;
-      }
-      case '4':{
-        String NewValue = readNewValue("mobile APN", 10, &timeout);
-        if (NewValue != "") { flashData.mobileAPN = NewValue; }
-        break;
-      }
-      case '5':{
-        String NewValue = readNewValue("mobile USER", 10, &timeout);
-        if (!timeout) { flashData.mobileUSER = NewValue; }
-        break;
-      }
-      case '6':{
-        String NewValue = readNewValue("mobile PASSWD", 10, &timeout);
-        if (!timeout) { flashData.mobilePASSWD = NewValue; }
-        break;
-      }
-      case '7':{
-        String NewValue = readNewValue("mobile PIN", 10, &timeout);
-        if (!timeout) { flashData.mobilePIN = NewValue; }
-        break;
-      }
-      case 'w':{
-        break;
-      }
-      //#endif
-      default:
-        break;
+  if (Rs232in.indexOf('x') != -1){
+    Serial.println("hook entered.");
+    loadFlashData(&flashData, &flashRawData);
+    char key = ' ';
+    bool leave = false;
+    while(!leave){
+      printMenu(&flashData);
+      delay(100);
+      key = getSerialKey();
+      bool timeout = false;
+      leave = handleMenu(key);
+      for(int i=0; i<5; i++) { Serial.println(""); }
     }
-    for(int i=0; i<5; i++) { Serial.println(""); }
-    printFlashDataMenu(&flashData);
+    Serial.println("leave hook");
+  } else {
+    Serial.println("start Heidi - application");
   }
+  delay(100);
+  #if DEBUG_LEVEL == DEBUG_LEVEL_0
+  Serial.end();
+  #endif
 }
 
-void printFlashDataMenu(t_flashData* flashData){
-  Serial.println("");
-  Serial.println("<----------------- Heidi set persistent data ---------->");
-  Serial.println("");
-  Serial.println("1: set herd   ID");
-  if (flashData->herdeID == FLASH_INT_UNSET){
-    #ifdef HEIDI_HERDE
-    flashData->herdeID = HEIDI_HERDE;
-    Serial.println("   \"" + intString4(flashData->herdeID) + "\" (*)");
-    #else
-    Serial.println("   -");
-    #endif
-  } else {
-    Serial.println("   \"" + intString4(flashData->herdeID) + "\"");
-  }
-  Serial.println("2: set animal ID");
-  if (flashData->animalID == FLASH_INT_UNSET){
-    #ifdef HEIDI_ANIMAL
-    flashData->animalID = HEIDI_ANIMAL;
-    Serial.println("   \"" + intString4(flashData->animalID) + "\" (*)");
-    #else
-    Serial.println("   -");
-    #endif
-  } else {
-    Serial.println("   \"" + intString4(flashData->animalID) + "\"");
-  }
-  Serial.println("3: set push data web address");
-  if (flashData->pushDataWebAddress == FLASH_STR_UNSET){
-    #ifdef HEIDI_SERVER_PUSH_URL
-    flashData->pushDataWebAddress = HEIDI_SERVER_PUSH_URL;
-    Serial.println("   \"" + flashData->pushDataWebAddress + "\" (*)");
-    #else
-    Serial.println("   -");
-    #endif
-  } else {
-    Serial.println("   \"" + flashData->pushDataWebAddress + "\"");
-  }
-  Serial.println("4: set mobile APN");
-  if (flashData->mobileAPN == FLASH_STR_UNSET){
-    #ifdef HEIDI_MOBILE_APN
-    flashData->mobileAPN = HEIDI_MOBILE_APN;
-    Serial.println("   \"" + flashData->mobileAPN + "\" (*)");
-    #else
-    Serial.println("   -");
-    #endif
-  } else {
-    Serial.println("   \"" + flashData->mobileAPN + "\"");
-  }
-  Serial.println("5: set mobile USER");
-  if (flashData->mobileUSER == FLASH_STR_UNSET){
-    #ifdef HEIDI_MOBILE_USER
-    if (HEIDI_MOBILE_USER == FLASH_STR_UNSET){
-      Serial.println("   \"" + flashData->mobileUSER + "\"");
-    } else {     flashData->mobileUSER = HEIDI_MOBILE_USER;
-      Serial.println("   \"" + flashData->mobileUSER + "\" (*)");
+void printMenu(t_flashData* flashData){
+  switch(currentMenu) {
+    case HEIDI_MENU_MAIN:{
+      printMainMenu(flashData);
+      break;
     }
-    #else
-    Serial.println("   -");
-    #endif
-  } else {
-    Serial.println("   \"" + flashData->mobileUSER + "\"");
-  }
-  Serial.println("6: set mobile PASSWD");
-  if (flashData->mobilePASSWD == FLASH_STR_UNSET){
-    #ifdef HEIDI_MOBILE_PASSWD
-    if (HEIDI_MOBILE_PASSWD == FLASH_STR_UNSET){
-      Serial.println("   \"" + flashData->mobilePASSWD + "\"");
-    } else {
-      flashData->mobilePASSWD = HEIDI_MOBILE_PASSWD;
-      Serial.println("   \"" + flashData->mobilePASSWD + "\" (*)");
+    case HEIDI_MENU_IDENTITY:{
+      printIdentityMenu(flashData);
+      break;
     }
-    #else
-    Serial.println("   -");
-    #endif
-  } else {
-    Serial.println("   \"" + flashData->mobilePASSWD + "\"");
-  }
-  Serial.println("7: set SIM PIN");
-  if (flashData->mobilePIN == FLASH_STR_UNSET){
-    #ifdef HEIDI_SIM_PIN
-    if (HEIDI_SIM_PIN == FLASH_STR_UNSET){
-      Serial.println("   \"" + flashData->mobilePIN + "\"");
-    } else {
-      flashData->mobilePIN = HEIDI_SIM_PIN;
-      Serial.println("   \"" + flashData->mobilePIN + "\" (*)");
+    case HEIDI_MENU_WEB_INTERFACE:{
+      printWebInterfaceMenu(flashData);
+      break;
     }
-    #else
-    Serial.println("   -");
-    #endif
-  } else {
-    Serial.println("   \"" + flashData->mobilePIN + "\"");
+    default:
+      break;
+
   }
-  Serial.println("w: write data");
-  Serial.println("q: quit");
+}
+bool handleMenu(char key){
+  switch(currentMenu) {
+    case HEIDI_MENU_MAIN:{
+      return handleMainMenu(key);
+      break;
+    }
+    case HEIDI_MENU_IDENTITY:{
+      return handleIdentityMenu(key);
+      break;
+    }
+    case HEIDI_MENU_WEB_INTERFACE:{
+      return handleWebInterfaceMenu(key);
+      break;
+    }
+    default:
+      break;
+  }
+  return false;
+}
+
+void printMainMenu(t_flashData* flashData){
   Serial.println("");
-  Serial.println("(*) values taken from default settings");
+  Serial.println("<----------------- Heidi main menu ----------------->");
+  Serial.println("");
+  Serial.println("1: identity settings");
+  Serial.println("2: web interface settings");
+  Serial.println("3: test measuring - not implemented yet");
+  Serial.println("");
+  Serial.println("w: write data to flash");
+  printFooter();
+}
+/*
+ * returns "true" if "quit" was chosen
+ */
+bool handleMainMenu(char key){
+  switch(key) {
+    case '1':{
+      currentMenu = HEIDI_MENU_IDENTITY;
+      break;
+    }
+    case '2':{
+      currentMenu = HEIDI_MENU_WEB_INTERFACE;
+      break;
+    }
+    case 'w':{
+      writeFlashData(&flashData, &flashRawData);
+      break;
+    }
+    case 'q':{
+      return true;
+    }
+    default:
+      break;
+  }
+  return false;
+}
+
+void printIdentityMenu(t_flashData* flashData){
+  Serial.println("");
+  Serial.println("<----------------- Heidi identity settings ----------------->");
+  Serial.println("");
+  Serial.println("a: set herd   ID");
+  Serial.println("   \"" + intString4(flashData->herdeID) + "\"");
+  Serial.println("b: set animal ID");
+  Serial.println("   \"" + intString4(flashData->animalID) + "\"");
+  printFooter();
+}
+
+bool handleIdentityMenu(char key){
+  bool timeout = false;
+  switch(key) {
+    case 'a':{
+      String NewValue = readNewValue("herd ID", &timeout);
+      if (!timeout) { flashData.herdeID = int16_t(NewValue.toInt()); }
+      break;
+    }
+    case 'b':{
+      String NewValue = readNewValue("animal ID", &timeout);
+      if (!timeout) { flashData.animalID = int16_t(NewValue.toInt()); }
+      break;
+    }
+    case 'q':{
+      currentMenu = HEIDI_MENU_MAIN;
+      break;
+    }
+    default:
+      break;
+  }
+  return false;
+}
+
+
+void printWebInterfaceMenu(t_flashData* flashData){
+  Serial.println("");
+  Serial.println("<----------------- Heidi web interface settings ----------------->");
+  Serial.println("");
+  Serial.println("a: set push data web address");
+  Serial.println("   \"" + flashData->pushDataWebAddress + "\"");
+  Serial.println("b: set mobile APN");
+  Serial.println("   \"" + flashData->mobileAPN + "\"");
+  Serial.println("c: set mobile USER");
+  Serial.println("   \"" + flashData->mobileUSER + "\"");
+  Serial.println("d: set mobile PASSWD");
+  Serial.println("   \"" + flashData->mobilePASSWD + "\"");
+  Serial.println("e: set SIM PIN");
+  Serial.println("   \"" + flashData->mobilePIN + "\"");
+  printFooter();
+}
+
+bool handleWebInterfaceMenu(char key){
+  bool timeout = false;
+  switch(key) {
+    case 'a':{
+      String NewValue = readNewValue("push data web address", &timeout);
+      if (!timeout) { flashData.pushDataWebAddress = NewValue; }
+      break;
+    }
+    case 'b':{
+      String NewValue = readNewValue("mobile APN", &timeout);
+      if (NewValue != "") { flashData.mobileAPN = NewValue; }
+      break;
+    }
+    case 'c':{
+      String NewValue = readNewValue("mobile USER", &timeout);
+      if (!timeout) { flashData.mobileUSER = NewValue; }
+      break;
+    }
+    case 'd':{
+      String NewValue = readNewValue("mobile PASSWD", &timeout);
+      if (!timeout) { flashData.mobilePASSWD = NewValue; }
+      break;
+    }
+    case 'e':{
+      String NewValue = readNewValue("mobile PIN", &timeout);
+      if (!timeout) { flashData.mobilePIN = NewValue; }
+      break;
+    }
+    case 'q':{
+      currentMenu = HEIDI_MENU_MAIN;
+      break;
+    }
+    default:
+      break;
+  }
+  return false;
+}
+
+
+void printFooter(void){
+  if(currentMenu == HEIDI_MENU_MAIN){
+    Serial.println("q: quit");
+  } else {
+    Serial.println("q: back");
+    Serial.println("");
+    Serial.println("(*) value taken from default settings");
+  }
   Serial.println("");
   Serial.println("<------------------------------------------------------>");
   Serial.println("");
 }
 
-void loadFlashData(t_flashData* flashData, t_flashRawData* flashRawData){
+bool loadFlashData(t_flashData* flashData, t_flashRawData* flashRawData){
+  bool good = false;
+  int  p = 0;
   byte* rawData = (byte*)flashRawData;
-  Serial.println("EEPROM load data length: " + String(sizeof(t_flashRawData)));
-  for(int i=0; i<sizeof(t_flashRawData); i++){
-    rawData[i] = EEPROM.read(i);
+  _D(DebugPrintln("EEPROM load data length: " + String(sizeof(t_flashRawData)), DEBUG_LEVEL_2);)
+  for(int t=0; t<2; t++){
+    for(int i=0; i<sizeof(t_flashRawData); i++){
+      rawData[i] = EEPROM.read(p);
+      p++;
+    }
+    if (checkData((t_flashRawData*)rawData)) { good = true; break; }
+    _D(DebugPrintln("EEPROM data set " + String(t) + " not valid!", DEBUG_LEVEL_1);)
   }
-  flashData->animalID = getFlashInt(flashRawData->animalID);
-  flashData->herdeID  = getFlashInt(flashRawData->herdeID);
-  flashData->mobileAPN = getFlashString(flashRawData->mobileAPN, sizeof(flashRawData->mobileAPN));
-  flashData->mobilePASSWD = getFlashString(flashRawData->mobilePASSWD, sizeof(flashRawData->mobilePASSWD));
-  flashData->mobilePIN = getFlashString(flashRawData->mobilePIN, sizeof(flashRawData->mobilePIN));
-  flashData->mobileUSER = getFlashString(flashRawData->mobileUSER, sizeof(flashRawData->mobileUSER));
-  flashData->pushDataWebAddress = getFlashString(flashRawData->pushDataWebAddress, sizeof(flashRawData->pushDataWebAddress));
+  if(good) {
+    flashData->animalID = getFlashInt(flashRawData->animalID);
+    flashData->herdeID  = getFlashInt(flashRawData->herdeID);
+    flashData->mobileAPN = getFlashString(flashRawData->mobileAPN, sizeof(flashRawData->mobileAPN));
+    flashData->mobilePASSWD = getFlashString(flashRawData->mobilePASSWD, sizeof(flashRawData->mobilePASSWD));
+    flashData->mobilePIN = getFlashString(flashRawData->mobilePIN, sizeof(flashRawData->mobilePIN));
+    flashData->mobileUSER = getFlashString(flashRawData->mobileUSER, sizeof(flashRawData->mobileUSER));
+    flashData->pushDataWebAddress = getFlashString(flashRawData->pushDataWebAddress, sizeof(flashRawData->pushDataWebAddress));
+  }
+  return good;
+}
+
+void writeFlashData(t_flashData* flashData, t_flashRawData* flashRawData){
+  byte* rawData = (byte*)flashRawData;
+  int  p = 0;
+  _D(DebugPrintln("EEPROM write data length: " + String(sizeof(t_flashRawData)), DEBUG_LEVEL_2);)
+  setFlashInt(flashRawData->animalID, flashData->animalID);
+  setFlashInt(flashRawData->herdeID, flashData->herdeID);
+  setFlashString(flashRawData->mobileAPN, sizeof(flashRawData->mobileAPN), flashData->mobileAPN);
+  setFlashString(flashRawData->mobilePASSWD, sizeof(flashRawData->mobilePASSWD), flashData->mobilePASSWD);
+  setFlashString(flashRawData->mobilePIN, sizeof(flashRawData->mobilePIN), flashData->mobilePIN);
+  setFlashString(flashRawData->mobileUSER, sizeof(flashRawData->mobileUSER),flashData->mobileUSER);
+  setFlashString(flashRawData->pushDataWebAddress, sizeof(flashRawData->pushDataWebAddress), flashData->pushDataWebAddress);
+  for(int t=0; t<2; t++){
+    for(int i=t; i<sizeof(t_flashRawData); i++){
+      if(EEPROM.read(i) != rawData[i]){ EEPROM.write(p,rawData[i]); }
+      p++;
+    }
+    setCheckSum((t_flashRawData*)rawData);
+    if (!EEPROM.commit()){_D(DebugPrintln("EEPROM writing data set " + String(t) + " failed!", DEBUG_LEVEL_1);)}
+  }
 }
 
 String getFlashString(byte* buffer, int len){
@@ -244,28 +332,72 @@ void clearFlashString(byte* buffer, int len){
   for(int i = 0; i<len; i++) { buffer[i] = 0xff; }
 }
 
-String readNewValue(const String prompt, char terminatior, bool* timeout){
+String readNewValue(const String prompt, bool* timeout){
   String NewValue = "";
+  char c = 0;
   while(Serial.available() > 0){ Serial.read(); }
   Serial.println("set new " + prompt + ":");
-  Serial.setTimeout(60000);
   int t = millis();
   *timeout = false;
-  NewValue = Serial.readStringUntil(terminatior);
+  while ((millis() -t < 60000) && (c != 10) && (c != 13)){
+    while (Serial.available() > 0){
+      c = Serial.read();
+      if (c >= 32) {
+        Serial.print(c);
+        NewValue += c;
+      }
+    }
+    delay(1);
+  }
+  Serial.println("");
   if ((millis() - t) >= 60000) *timeout = true;
   Serial.setTimeout(100);
   delay(1);
   while(Serial.available() > 0){ Serial.read(); delay(1);}
+  NewValue.trim();
   return(NewValue);
 }
 
 char getSerialKey(void){
+  while(Serial.available() > 0){ Serial.read(); delay(1);}
   while(Serial.available() == 0){
     delay(1);
   }
   char ch = (char)Serial.read();
   while(Serial.available() > 0){ Serial.read(); delay(1);}
   return ch;
+}
+
+bool checkData(t_flashRawData* flashRawData){
+  uint8_t a = 0;
+  uint8_t b = 0;
+  uint8_t* buffer;
+  buffer = (uint8_t*)flashRawData;
+  int l = sizeof(t_flashRawData)-2;
+  for(int i = 0; i < l; i++){
+    a += buffer[i];
+    b += a;
+  }
+  if(a == buffer[l]){
+    if(b == buffer[l+1]){
+      return true;
+    }
+  }
+  return false;
+}
+
+void setCheckSum(t_flashRawData* flashRawData){
+  uint8_t a = 0;
+  uint8_t b = 0;
+  uint8_t* buffer;
+  buffer = (uint8_t*)flashRawData;
+  int l = sizeof(t_flashRawData)-2;
+  for(int i = 0; i < l; i++){
+    a += buffer[i];
+    b += a;
+  }
+  a = buffer[l];
+  b = buffer[l+1];
 }
 
 _D(
--- backend/heidi-flash.h
+++ backend/heidi-flash.h
@@ -17,12 +17,13 @@ typedef __attribute__((__packed__)) struct _t_flashRawData{
  byte herdeID[2];
  byte animalID[2];
  //#ifdef GSM_MODULE
- byte pushDataWebAddress[128];
+ byte pushDataWebAddress[256];
  byte mobileAPN[64];
  byte mobileUSER[16];
  byte mobilePASSWD[16];
  byte mobilePIN[8];
  //#endif
+ byte checkSum[2];
 }t_flashRawData;
 
 typedef __attribute__((__packed__)) struct _t_flashData{
@@ -37,10 +38,28 @@ typedef __attribute__((__packed__)) struct _t_flashData{
  //#endif
 }t_flashData;
 
+typedef enum{
+  HEIDI_MENU_MAIN,
+  HEIDI_MENU_IDENTITY,
+  HEIDI_MENU_WEB_INTERFACE
+}t_heidiMenu;
 
 void setupFlashData(bool powerOnReset);
-void printFlashDataMenu(t_flashData* flashData);
-void loadFlashData(t_flashData* flashData, t_flashRawData* flashRawData);
+
+void handleHook();
+void printMenu(t_flashData* flashData);
+bool handleMenu(char key);
+void printMainMenu(t_flashData* flashData);
+bool handleMainMenu(char key);
+void printIdentityMenu(t_flashData* flashData);
+bool handleIdentityMenu(char key);
+void printWebInterfaceMenu(t_flashData* flashData);
+bool handleWebInterfaceMenu(char key);
+void printFooter(void);
+
+void loadDefaultData(t_flashData* flashData);
+bool loadFlashData(t_flashData* flashData, t_flashRawData* flashRawData);
+void writeFlashData(t_flashData* flashData, t_flashRawData* flashRawData);
 
 void setFlashString(byte* ,int ,const String );
 String getFlashString(byte* ,int );
@@ -48,8 +67,10 @@ int16_t getFlashInt(byte* );
 void setFlashInt(byte* buffer, int16_t );
 void clearFlashString(byte*, int );
 
-String readNewValue(const String prompt, char terminatior, bool* timeout);
+String readNewValue(const String prompt, bool* timeout);
 char getSerialKey(void);
+bool checkData(t_flashRawData* flashRawData);
+void setCheckSum(t_flashRawData* flashRawData);
 
 _D( void printFlashString(byte* , int ); )
 
